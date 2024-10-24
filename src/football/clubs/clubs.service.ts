ade070c85306d0788822a615d46601fa07def190
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-10-24 10:10:20 +0800

    clubs services

--- src/football/clubs/clubs.service.ts
+++ src/football/clubs/clubs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateClubDto } from './dto/create-club.dto';
 import { UpdateClubDto } from './dto/update-club.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,6 +13,15 @@ export class ClubsService {
   ) {}
 
   async create(data: Partial<Club>): Promise<Club> {
+    const existingClubName = await this.clubRepository.findOne({ where: { name: data.name } });
+    const existingSlug = await this.clubRepository.findOne({ where: { slug: data.slug } });
+
+    if (existingClubName) {
+      throw new ConflictException('Club name already exists');
+    } else if (existingSlug) {
+      throw new ConflictException('Slug name already exists');
+    }
+    
     const result = this.clubRepository.create(data);
     return this.clubRepository.save(result);
   }
@@ -31,7 +40,27 @@ export class ClubsService {
 
   async update(id: number, data: Partial<Club>): Promise<Club> {
     const result = await this.findById(id);
-    return this.clubRepository.save({ ...result, ...data });
+
+    if (!result) {
+      throw new NotFoundException('Club not found');
+    }
+
+    if (data.name && data.name !== result.name) {
+      const existingClubName = await this.clubRepository.findOne({ where: { name: data.name } });
+      if (existingClubName && existingClubName.id !== id) {
+        throw new ConflictException('Club name already exists');
+      }
+    }
+
+    if (data.slug && data.slug !== result.slug) {
+      const existingSlug = await this.clubRepository.findOne({ where: { slug: data.slug } });
+      if (existingSlug && existingSlug.id !== id) {
+        throw new ConflictException('Slug name already exists');
+      }
+    }
+
+    Object.assign(result, data);
+    return this.clubRepository.save(result);
   }
 
   async remove(id: number): Promise<void> {
